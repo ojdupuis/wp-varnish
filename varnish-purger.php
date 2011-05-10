bdee119579633964dc7bc6df944382a15021a0a0
Olivier Dupuis <dupuis.olivier@lemonde.fr>
2011-05-10 17:39:13 +0200

    Correct admin bug for server configuration

--- varnish-purger.php
+++ varnish-purger.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WordPress Varnish Purger
 Plugin URI: https://github.com/ojdupuis/wp-varnish/tree/Support_Extensions
-Version: 0.91
+Version: 0.93
 Author: <a href="http://github.com/ojdupuis/">Olivier Dupuis</a> for Le Monde interactif
 Description: A plugin for purging Varnish cache when content is published or edited. Based on Varsnish plugins by pkhamre, wfelipe, eitch, but heavily forked for extensibility.
 
@@ -88,9 +88,7 @@ class WPVarnishPurger {
       
         
   }
-  
-  
-
+ 
   function WPVarnishPurgerLocalization() {
     load_plugin_textdomain('varnishpurger',false,'varnishpurger/lang');
   }
