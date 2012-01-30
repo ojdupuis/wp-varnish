e8eab9dabb1d3f3d87f35bc46eb85dec03aec6d6
Olivier Dupuis <dupuis.olivier@lemonde.fr>
2012-01-30 14:03:47 +0100

    Réactivation css perso

--- plugins/varnish_wp-cssperso.php
+++ plugins/varnish_wp-cssperso.php
@@ -1,19 +1,25 @@
 <?php
 /*
- * Support for personnalized css 
+ * Support for personnalized css
  * Url none
  * Version 1
  */
 class WPVarnishPurger_WPCssPerso extends WPVarnishPurgerCore {
-   
+
    function mustActivate(){
-      return true; 
-   }      
-  
-   function addActions(){      
-        add_action('update_option_css_perso', array(&$this, 'WPVarnishPurgerPurgeAll'), 99);     
+      return true;
    }
-   
+
+   function addActions(){
+        add_action('update_option_css_perso', array(&$this, 'WPVarnishPurgerPurgeCssPerso'), 99);
+
+   }
+
+   // Purge Ajax Calendar for a post
+   function WPVarnishPurgerPurgeCssPerso(){
+        $this->WPVarnishPurgerPurgeObject(get_bloginfo('wpurl').'wp-content/themes/common/css_perso.php');
+  }
+
 }
 $WPVarnishPurgerCssPerso = & new WPVarnishPurger_WPCssPerso();
 
